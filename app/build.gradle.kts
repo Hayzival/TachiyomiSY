1822f9e4e223cdb83a1acc3bcbb4755f51317abb
Hayzival <bryanblindorard@gmail.com>
2026-06-12 18:53:03 +0200

    My version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
     namespace = "eu.kanade.tachiyomi"
 
     defaultConfig {
-        applicationId = "eu.kanade.tachiyomi.sy"
+        applicationId = "eu.hayzival.tachiyomi.sy"
 
         versionCode = 77
         versionName = "1.12.0"
